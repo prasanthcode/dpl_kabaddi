cb0b1996c3d1a8177a9d8718c2765e1b562d6c35
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 22:30:25 +0530

    feat: add skeleton loader for match component

--- src/components/MatchSkeleton.js
+++ src/components/MatchSkeleton.js
@@ -1,50 +1,16 @@
 import React from "react";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import "../styles/Match.css";
 
-const MatchSkeleton = () => {
+export default function MatchSkeleton() {
   return (
-    <div className="match_container">
-      <div className="d_match">
-        <div className="single_match_wrapper">
-          <div className="bg-container">
-            <div
-              className="single_match view_recent skeleton-container"
-              style={{
-                backgroundColor: "rgb(8, 43, 109)",
-                padding: "20px",
-                borderRadius: "10px",
-              }}
-            >
-              <h4>
-                <Skeleton width={100} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-              </h4>
-              <div className="vs_container" style={{ display: "flex", justifyContent: "space-between" }}>
-                <div className="img_wrap" style={{ textAlign: "center" }}>
-                  <Skeleton circle width={80} height={80} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                  <Skeleton width={100} height={10} style={{ marginTop: "10px" }} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                  <Skeleton width={40} height={10} style={{ marginTop: "5px" }} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                </div>
-                <div className="time" style={{ backgroundColor: "rgb(34, 139, 69)", color: "white", padding: "10px 20px", borderRadius: "5px" }}>
-                  <Skeleton width={30} height={10} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                </div>
-                <div className="img_wrap" style={{ textAlign: "center" }}>
-                  <Skeleton circle width={80} height={80} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                  <Skeleton width={100} height={10} style={{ marginTop: "10px" }} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                  <Skeleton width={40} height={10} style={{ marginTop: "5px" }} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                </div>
-              </div>
-              <Skeleton width={250} height={15} style={{ margin: "10px auto" }} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-              <div className="mat" style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
-                <Skeleton width={80} height={10} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                <Skeleton width={100} height={10} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="match_skeleton">
+      <div className="skeleton_card">
+        <div className="skeleton_team"></div>
+        <div className="skeleton_vs"></div>
+        <div className="skeleton_team"></div>
       </div>
+      <div className="skeleton_toggle"></div>
+      <div className="skeleton_content"></div>
     </div>
   );
-};
-
-export default MatchSkeleton;
+}
