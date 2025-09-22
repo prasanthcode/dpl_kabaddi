43f95346ce27d410c2eaf9c7a0aea42cc1f04e44
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 22:29:26 +0530

    refactor: adjust skeleton loader according to updated match list design

--- src/components/MatchListSkeleton.js
+++ src/components/MatchListSkeleton.js
@@ -1,28 +1,70 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import "../styles/Matches.css";
 
-const MatchListSkeleton = ({isHomePage}) => {
+const MatchListSkeleton = ({ isHomePage }) => {
   return (
-    <div className="match_container">
-      {[...Array(isHomePage?1:3)].map((_, index) => (
-        <div className="d_match" key={index} style={{ backgroundColor: "var(--primary-dark)",  marginBottom: "20px" }}>
-          <Skeleton height={30} width={200} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" style={{ marginBottom: "20px" }} />
-          {[...Array(isHomePage?1:2)].map((_, idx) => (
-            <div className="single_match_wrapper" key={idx}>
-              <div className="single_match skeleton-container" style={{ backgroundColor: "rgb(8, 43, 109)", margin:"20px 0"}}>
+    <div className="matches-container matches-skeleton-container">
+      {[...Array(isHomePage ? 1 : 3)].map((_, index) => (
+        <div className="matches-skeleton-group" key={index}>
+          <Skeleton
+            height={30}
+            width={200}
+            baseColor="rgb(6, 35, 90)"
+            highlightColor="rgb(12, 55, 130)"
+            style={{ marginBottom: "20px" }}
+          />
+          {[...Array(isHomePage ? 1 : 2)].map((_, idx) => (
+            <div className="matches-single-wrapper" key={idx}>
+              <div className="matches-single matches-skeleton-single">
                 <h4>
-                  <Skeleton width={100} height={20} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
+                  <Skeleton
+                    width={100}
+                    height={20}
+                    baseColor="rgb(6, 35, 90)"
+                    highlightColor="rgb(12, 55, 130)"
+                  />
                 </h4>
-                <div className="vs_container" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                  <div className="img_wrap" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Skeleton circle width={80} height={80} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                    <Skeleton width={100} height={10} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" style={{ marginTop: "10px" }} />
+
+                <div className="matches-vs-container matches-skeleton-vs">
+                  <div className="matches-img-wrap matches-skeleton-img">
+                    <Skeleton
+                      circle
+                      width={80}
+                      height={80}
+                      baseColor="rgb(6, 35, 90)"
+                      highlightColor="rgb(12, 55, 130)"
+                    />
+                    <Skeleton
+                      width={100}
+                      height={10}
+                      baseColor="rgb(6, 35, 90)"
+                      highlightColor="rgb(12, 55, 130)"
+                      style={{ marginTop: "10px" }}
+                    />
                   </div>
-                  <Skeleton width={50} height={20} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                  <div className="img_wrap" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                    <Skeleton circle width={80} height={80} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" />
-                    <Skeleton width={100} height={10} baseColor="rgb(6, 35, 90)" highlightColor="rgb(12, 55, 130)" style={{ marginTop: "10px" }} />
+                  <Skeleton
+                    width={50}
+                    height={20}
+                    baseColor="rgb(6, 35, 90)"
+                    highlightColor="rgb(12, 55, 130)"
+                  />
+                  <div className="matches-img-wrap matches-skeleton-img">
+                    <Skeleton
+                      circle
+                      width={80}
+                      height={80}
+                      baseColor="rgb(6, 35, 90)"
+                      highlightColor="rgb(12, 55, 130)"
+                    />
+                    <Skeleton
+                      width={100}
+                      height={10}
+                      baseColor="rgb(6, 35, 90)"
+                      highlightColor="rgb(12, 55, 130)"
+                      style={{ marginTop: "10px" }}
+                    />
                   </div>
                 </div>
               </div>
