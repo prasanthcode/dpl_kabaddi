b304d61a0f725c219d6df348c22dc9c71c0d60a0
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 22:35:17 +0530

    style(stats): add CSS for stats component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React from "react";
 
-export default function Stats({total}) {
+export default function Stats({ total }) {
   return (
-    <div className="stats_wrap">
-          <div className="stats_row">
-            <span>{total.teamA.score}</span> <span>Total Points</span><span>{total.teamB.score}</span>
-          </div>
-          <div className="stats_row">
-            <span>{total.teamA.totalRaidPoints}</span> <span>Total Raid Points</span><span>{total.teamB.totalRaidPoints}</span>
-          </div>
-          <div className="stats_row">
-            <span>{total.teamA.totalDefensePoints}</span> <span>Total Defense Points</span><span>{total.teamB.totalDefensePoints}</span>
-          </div>
-          <div className="stats_row">
-            <span>{total.teamA.score - total.teamA.totalDefensePoints - total.teamA.totalRaidPoints}</span>
-            <span>Extra Points</span>
-            <span>{total.teamB.score - total.teamB.totalDefensePoints - total.teamB.totalRaidPoints}</span>
-          </div>
-        </div>
-  )
+    <div className="match_stats_wrap">
+      <div className="match_stats_row">
+        <span>{total.teamA.score}</span> <span>Total Points</span>
+        <span>{total.teamB.score}</span>
+      </div>
+      <div className="match_stats_row">
+        <span>{total.teamA.totalRaidPoints}</span>{" "}
+        <span>Total Raid Points</span>
+        <span>{total.teamB.totalRaidPoints}</span>
+      </div>
+      <div className="match_stats_row">
+        <span>{total.teamA.totalDefensePoints}</span>{" "}
+        <span>Total Defense Points</span>
+        <span>{total.teamB.totalDefensePoints}</span>
+      </div>
+      <div className="match_stats_row">
+        <span>
+          {total.teamA.score -
+            total.teamA.totalDefensePoints -
+            total.teamA.totalRaidPoints}
+        </span>
+        <span>Extra Points</span>
+        <span>
+          {total.teamB.score -
+            total.teamB.totalDefensePoints -
+            total.teamB.totalRaidPoints}
+        </span>
+      </div>
+    </div>
+  );
 }
