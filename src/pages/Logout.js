3e47a0595776eb1476dcc86a3345744757757c4b
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 23:22:37 +0530

    style: add css to logout component

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Button from "@mui/material/Button";
 
 export default function Logout() {
   const { logout } = useContext(AuthContext);
@@ -11,5 +13,20 @@ export default function Logout() {
     navigate("/login");
   };
 
-  return <button onClick={handleLogout}>Logout</button>;
+  return (
+    <Button
+      onClick={handleLogout}
+      variant="contained"
+      color="error"
+      startIcon={<LogoutIcon />}
+      sx={{
+        borderRadius: 2,
+        textTransform: "none",
+        fontWeight: 500,
+        px: 2,
+      }}
+    >
+      Logout
+    </Button>
+  );
 }
