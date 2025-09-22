2873a5250ac594b2185c1f69103b9d7d65a98f1c
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 22:28:16 +0530

    feat(match list): show weekday along with date and match type

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,112 +1,123 @@
-import { Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Matches.css";
 
-const MatchList = ({ matches = [], status, isAdmin,isHomePage }) => {
-  // Ensure matches is an array before using forEach
+const MatchList = ({ matches = [], status, isHomePage }) => {
   const validMatches = Array.isArray(matches) ? matches : [];
-console.log(matches);
-  // Create an object to store matches grouped by date
   const matchesByDate = validMatches.reduce((acc, match) => {
     const date = new Date(match.date).toLocaleDateString("en-US", {
       year: "numeric",
       month: "long",
       day: "numeric",
+      weekday: "long",
     });
-
     if (!acc[date]) acc[date] = [];
     acc[date].push(match);
     return acc;
   }, {});
 
   return (
-    <div className="match_container" style={{minHeight: !isHomePage ? "100vh":""}}>
+    <div
+      className="matches-container"
+      style={{ minHeight: !isHomePage ? "100vh" : "" }}
+    >
       {Object.entries(matchesByDate).map(([date, matches]) => (
-        <div className="d_match" key={date}>
+        <div className="matches-date-group" key={date}>
           <h2>
-  {status !== "live" ? (date && !isNaN(new Date(date)) ? date : "To be Announced") : <>Live <span className="live-dot"></span></>}
-</h2>
+            {matches[0].status !== "Ongoing" ? (
+              date && !isNaN(new Date(date)) ? (
+                date
+              ) : (
+                "To be Announced"
+              )
+            ) : (
+              <>
+                Live <span className="matches-live-dot"></span>
+              </>
+            )}
+          </h2>
 
           {matches.map((match, index) => (
-            <div className="single_match_wrapper" key={index}>
-              {isAdmin && (
-                <Link
-                  to={`/addscore/${match.matchId}`}
-                  className="edit_button"
-                  style={{ textDecoration: "none", color: "white" }}
-                >
-                  📝 Edit Match
-                </Link>
-              )}
-
+            <div className="matches-single-wrapper" key={index}>
               <Link
                 to={
-                  status === "upcoming"
+                  match.status === "Upcoming"
                     ? "/upcoming"
-                    : status === "completed"
+                    : match.status === "Completed"
                     ? `/recent/${match.matchId}`
                     : `/live/${match.matchId}`
                 }
                 style={{ textDecoration: "none", color: "white" }}
               >
-                <div className="single_match">
-                  <h4>{ match.matchType ? match.matchType :`Match ${match.matchNumber}`}</h4>
-                  {/* <h4>{ `Match `}</h4> */}
+                <div className="matches-single">
+                  <h4>
+                    {match.matchType
+                      ? match.matchType
+                      : `Match ${match.matchNumber}`}
+                  </h4>
 
-                  <div className="vs_container">
-                    <div className="img_wrap">
+                  <div className="matches-vs-container">
+                    <div className="matches-img-wrap">
                       <img src={match.teamA.logo} alt="" />
-                      <div className="team_1">
+                      <div className="matches-team">
                         <h3>{match.teamA.name}</h3>
-                        {status !== "upcoming" && (
-                          <span className="teama_score">{match.teamA.score}</span>
+                        {match.status !== "Upcoming" && (
+                          <span
+                            className={`teama_score ${
+                              match.teamA.score > match.teamB.score
+                                ? "winner"
+                                : match.teamA.score < match.teamB.score
+                                ? "loser"
+                                : ""
+                            }`}
+                          >
+                            {match.teamA.score}
+                          </span>
                         )}
                       </div>
                     </div>
+
                     <div
-                      className="time"
+                      className="matches-time"
                       style={{
-                        backgroundColor: status === "completed" ? "rgb(34, 139, 69)" : "white",
-                        color: status === "completed" ? "white" : "black",
+                        borderColor:
+                          match.status === "Completed"
+                            ? "green"
+                            : match.halfTime && match.status === "Ongoing"
+                            ? "red"
+                            : "white",
                       }}
                     >
-                     {
-  status === "completed" 
-    ? "FT" 
-    : status === "upcoming" 
-      ? "VS" 
-      : match.halfTime 
-        ? "HT" 
-        : "VS"
-}
+                      {match.status === "Completed"
+                        ? "FT"
+                        : match.status === "Upcoming"
+                        ? "VS"
+                        : match.halfTime
+                        ? "HT"
+                        : "VS"}
                     </div>
-                    <div className="img_wrap">
+
+                    <div className="matches-img-wrap">
                       <img src={match.teamB.logo} alt="" />
-                      <div className="team_1">
-                        {status !== "upcoming" && (
-                          <span className="teamb_score">{match.teamB.score}</span>
+                      <div className="matches-team">
+                        {match.status !== "Upcoming" && (
+                          <span
+                            className={`teamb_score ${
+                              match.teamB.score > match.teamA.score
+                                ? "winner"
+                                : match.teamB.score < match.teamA.score
+                                ? "loser"
+                                : ""
+                            }`}
+                          >
+                            {match.teamB.score}
+                          </span>
                         )}
                         <h3>{match.teamB.name}</h3>
                       </div>
                     </div>
                   </div>
                 </div>
-                {status==="live" && (
-  <Typography
-    variant="subtitle1" 
-    sx={{ 
-      fontWeight: "bold", 
-      color: "white", 
-      backgroundColor: "var(--primary-dark)", 
-      padding: "5px 10px",
-      fontSize:"15px", 
-      borderRadius: "5px",
-      display: "inline-block"
-    }}
-  >
-    ⚡ Score updates automatically, no need to refresh! 
-  </Typography>
-)}
               </Link>
             </div>
           ))}
