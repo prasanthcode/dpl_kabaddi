6c277a64c1536c85098d49f30b32e9b743cdef73
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 22:57:14 +0530

    feat(navbar): add underline for teams when team info is active and show completed matches page when there are no live matches

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,25 +1,76 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import useLiveMatchesCheck from "../../hooks/useLiveMatchesCheck";
 
 export default function Navbar() {
-  const location = useLocation(); // Track full URL changes
+  const location = useLocation();
   const path = location.pathname;
+  const { showLive } = useLiveMatchesCheck();
+
   return (
     <div className="buttons">
-      <Link to="/" style={{textDecoration:path==="/"?"underline" :"none",textUnderlineOffset: "6px",textDecorationThickness:"2px"}}>
-        <FontAwesomeIcon icon={faHome}  />
-        {/* <img src={banner} alt="" width={40} style={{borderRadius:"50%"}}/> */}
+      <Link
+        to="/"
+        style={{
+          textDecoration: path === "/" ? "underline" : "none",
+          textUnderlineOffset: "6px",
+          textDecorationThickness: "2px",
+        }}
+      >
+        <FontAwesomeIcon icon={faHome} />
       </Link>
-      <Link to="/live" style={{textDecoration:path==="/live" || path==="/upcoming" || path==="/completed"?"underline" :"none",textUnderlineOffset: "6px",textDecorationThickness:"2px"}}>Matches</Link>
-      <Link to="/teams" style={{textDecoration:path==="/teams"?"underline" :"none",textUnderlineOffset: "6px",textDecorationThickness:"2px"}}>Teams</Link>
-      <Link to="/standings" style={{textDecoration:path==="/standings"?"underline" :"none",textUnderlineOffset: "6px",textDecorationThickness:"2px"}}>Standings</Link>
-      <Link to="/playerstats"  style={{textDecoration:path==="/playerstats"?"underline" :"none",textUnderlineOffset: "6px",textDecorationThickness:"2px"}}>Stats</Link>
-      {/* <Link to="/">
-        <FontAwesomeIcon icon={faInstagram}  />
-      </Link> */}
+
+      <Link
+        to={showLive ? "/live" : "/completed"}
+        style={{
+          textDecoration:
+            path === "/live" || path === "/upcoming" || path === "/completed"
+              ? "underline"
+              : "none",
+          textUnderlineOffset: "6px",
+          textDecorationThickness: "2px",
+        }}
+      >
+        Matches
+      </Link>
+
+      <Link
+        to="/teams"
+        style={{
+          textDecoration:
+            path === "/teams" || path.startsWith("/team/")
+              ? "underline"
+              : "none",
+          textUnderlineOffset: "6px",
+          textDecorationThickness: "2px",
+        }}
+      >
+        Teams
+      </Link>
+
+      <Link
+        to="/standings"
+        style={{
+          textDecoration: path === "/standings" ? "underline" : "none",
+          textUnderlineOffset: "6px",
+          textDecorationThickness: "2px",
+        }}
+      >
+        Standings
+      </Link>
+
+      <Link
+        to="/playerstats"
+        style={{
+          textDecoration: path === "/playerstats" ? "underline" : "none",
+          textUnderlineOffset: "6px",
+          textDecorationThickness: "2px",
+        }}
+      >
+        Stats
+      </Link>
     </div>
   );
 }
