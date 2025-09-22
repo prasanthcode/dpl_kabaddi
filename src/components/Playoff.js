d74773cf42b78079c02cd5ffd07ef856cd760536
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 22:33:48 +0530

    style(playoff): add CSS for playoff component

--- src/components/Playoff.js
+++ src/components/Playoff.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Playoff() {
   return (
-   <>
-   <h3 style={{textAlign:"center"}}>DPL Boys Kabaddi Road To The Final</h3>
-   <img src="https://res.cloudinary.com/dbs1mjd6b/image/upload/v1742799674/WhatsApp_Image_2025-03-24_at_12.21.35_PM_klsnnj.jpg" alt="" style={{width:"100%"}} />
-   </>
-  )
+    <div style={{ maxWidth: "1200px", margin: "20px auto", padding: "0 10px" }}>
+      <h3 style={{ textAlign: "center", marginBottom: "20px" }}>
+        DPL Boys Kabaddi Road To The Final
+      </h3>
+      <img
+        src="https://res.cloudinary.com/dbs1mjd6b/image/upload/v1742799674/WhatsApp_Image_2025-03-24_at_12.21.35_PM_klsnnj.jpg"
+        alt="Road to the Final"
+        style={{
+          width: "100%",
+          height: "auto",
+          borderRadius: "8px",
+          boxShadow: "0 4px 15px rgba(0,0,0,0.3)",
+        }}
+      />
+    </div>
+  );
 }
