9966d3c028d2bbc35b41748f14b6fe4a5dd48cce
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-22 22:23:21 +0530

    feat(lineup): display profile pictures for players

--- src/components/LineUp.js
+++ src/components/LineUp.js
@@ -2,34 +2,60 @@ import React from "react";
 
 export default function LineUp({ stats }) {
   return (
-    <div className="line_up">
-      <h3>Top Raiders</h3>
-      {stats.topRaiders?.length > 0 ? (
-        stats.topRaiders.map((raider, index) => (
-          <div className="player" key={index}>
-            <div className="p_info">
-              <span>{raider.name}</span> <span>{raider.team}</span>
+    <div className="match_lineup">
+      <div className="top_players">
+        <h3>Top Raiders</h3>
+        {stats.topRaiders?.length > 0 ? (
+          stats.topRaiders.map((raider, index) => (
+            <div className="match_player" key={index}>
+              <div className="match_p_info">
+                <img
+                  src={
+                    raider.profilePic
+                      ? `${
+                          process.env.REACT_APP_CLOUDINARY_FILE_BASE_URL +
+                          raider.profilePic
+                        }.jpg`
+                      : "https://www.gravatar.com/avatar/?d=mp"
+                  }
+                  alt="No Pic"
+                />
+                <span>{raider.name}</span>
+              </div>
+              <span>{raider.totalRaidPoints}</span>
             </div>
-            <span>{raider.totalRaidPoints}</span>
-          </div>
-        ))
-      ) : (
-        <p className="empty-message">No top raiders yet</p>
-      )}
+          ))
+        ) : (
+          <p className="match_empty">No top raiders yet</p>
+        )}
+      </div>
+      <div className="top_players">
+        <h3>Top Defenders</h3>
+        {stats.topDefenders?.length > 0 ? (
+          stats.topDefenders.map((defender, index) => (
+            <div className="match_player" key={index}>
+              <div className="match_p_info">
+                <img
+                  src={
+                    defender.profilePic
+                      ? `${
+                          process.env.REACT_APP_CLOUDINARY_FILE_BASE_URL +
+                          defender.profilePic
+                        }.jpg`
+                      : "https://www.gravatar.com/avatar/?d=mp"
+                  }
+                  alt="No Pic"
+                />
 
-      <h3>Top Defenders</h3>
-      {stats.topDefenders?.length > 0 ? (
-        stats.topDefenders.map((defender, index) => (
-          <div className="player" key={index}>
-            <div className="p_info">
-              <span>{defender.name}</span> <span>{defender.team}</span>
+                <span>{defender.name}</span>
+              </div>
+              <span>{defender.totalDefensePoints}</span>
             </div>
-            <span>{defender.totalDefensePoints}</span>
-          </div>
-        ))
-      ) : (
-        <p className="empty-message">No top defenders yet</p>
-      )}
+          ))
+        ) : (
+          <p className="match_empty">No top defenders yet</p>
+        )}
+      </div>
     </div>
   );
 }
