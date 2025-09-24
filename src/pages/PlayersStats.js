134235ef48e822b83a67b71268ffdd3fd4943e59
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-09-24 22:30:49 +0530

    refactor: remove unused code

--- src/pages/PlayersStats.js
+++ src/pages/PlayersStats.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
