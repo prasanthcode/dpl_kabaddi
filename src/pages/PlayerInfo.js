be40fd686119812bd7fdcd20dcb83f8bb4172855
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-10-08 14:16:20 +0530

    feat: show not yet played for the player

--- src/pages/PlayerInfo.js
+++ src/pages/PlayerInfo.js
@@ -1,4 +1,3 @@
-// import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { usePlayerInfo } from "../hooks/usePlayerInfo";
@@ -9,33 +8,54 @@ export default function PlayerInfo() {
 
   if (error) return <p>Error: {error}</p>;
 
+  if (loading) return <LoadingSpinner />;
+
+  const hasPlayed =
+    player.totalStats.totalRaidPoints > 0 ||
+    player.totalStats.totalDefensePoints > 0 ||
+    player.totalStats.totalPoints > 0 ||
+    player.totalStats.totalSuper10s > 0 ||
+    player.totalStats.totalHigh5s > 0 ||
+    player.totalStats.totalSuperRaids > 0 ||
+    player.matchStats.some(
+      (match) =>
+        match.raidPoints > 0 ||
+        match.defensePoints > 0 ||
+        match.totalPoints > 0 ||
+        match.super10 > 0 ||
+        match.high5 > 0 ||
+        match.superRaids > 0
+    );
+
   return (
-    <>
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <div className="main_player">
-          <div className="player_info">
+    <div className="main_player">
+      <div className="player_info">
+        <img
+          src={
+            player.profilePic ||
+            `${process.env.REACT_APP_CLOUDINARY_FILE_BASE_URL}g2xafebqt6zkwwuzdaiy.png`
+          }
+          alt={player.name}
+        />
+        <div className="name_team_info">
+          <div className="team_info">
             <img
               src={
-                player.profilePic ||
-                `${process.env.REACT_APP_CLOUDINARY_FILE_BASE_URL}g2xafebqt6zkwwuzdaiy.png`
+                player.team?.logo || "https://avatar.iran.liara.run/public/47"
               }
-              alt={player.name}
+              alt={player.team?.name}
             />
-            <div className="name_team_info">
-              <div className="team_info">
-                <img
-                  src={
-                    player.team?.logo ||
-                    "https://avatar.iran.liara.run/public/47"
-                  }
-                  alt={player.team?.name}
-                />
-              </div>
-              <h3>{player.name}</h3>
-            </div>
           </div>
+          <h3>{player.name}</h3>
+        </div>
+      </div>
+
+      {!hasPlayed ? (
+        <div className="not_played">
+          <h2 style={{color:"#abababff"}}>Not yet played</h2>
+        </div>
+      ) : (
+        <>
           <h2>Player Stats</h2>
           <div className="total_player_stats">
             {player.totalStats.totalRaidPoints > 0 && (
@@ -53,19 +73,19 @@ export default function PlayerInfo() {
             {player.totalStats.totalPoints > 0 && (
               <div className="stat_type">
                 <h1>{player.totalStats.totalPoints}</h1>
-                <span>Points</span>
+                <span>Total Points</span>
               </div>
             )}
             {player.totalStats.totalSuper10s > 0 && (
               <div className="stat_type">
                 <h1>{player.totalStats.totalSuper10s}</h1>
-                <span>Super10s</span>
+                <span>Super 10s</span>
               </div>
             )}
             {player.totalStats.totalHigh5s > 0 && (
               <div className="stat_type">
                 <h1>{player.totalStats.totalHigh5s}</h1>
-                <span>High5s</span>
+                <span>High 5s</span>
               </div>
             )}
             {player.totalStats.totalSuperRaids > 0 && (
@@ -75,83 +95,73 @@ export default function PlayerInfo() {
               </div>
             )}
           </div>
+
           <h2>Player Stats By Match</h2>
+          <div className="by_match">
+            {player.matchStats.map((match) => {
+              if (
+                match.raidPoints === 0 &&
+                match.defensePoints === 0 &&
+                match.totalPoints === 0 &&
+                match.super10 === 0 &&
+                match.high5 === 0 &&
+                match.superRaids === 0
+              ) {
+                return null;
+              }
 
-          {player.matchStats.some(
-            (match) =>
-              match.raidPoints > 0 ||
-              match.defensePoints > 0 ||
-              match.totalPoints > 0 ||
-              match.super10 > 0 ||
-              match.high5 > 0 ||
-              match.superRaids > 0
-          ) && (
-            <div className="by_match">
-              {player.matchStats.map((match) => {
-                if (
-                  match.raidPoints === 0 &&
-                  match.defensePoints === 0 &&
-                  match.totalPoints === 0 &&
-                  match.super10 === 0 &&
-                  match.high5 === 0 &&
-                  match.superRaids === 0
-                ) {
-                  return null;
-                }
-
-                return (
-                  <div key={match.matchId} className="total_player_stats">
-                    <div className="opponent">
-                      <img src={player.team.logo} alt={player.team.logo} />
-                      <span>VS</span>
-                      <img
-                        src={match.opponentTeam.logo}
-                        alt={match.opponentTeam.name}
-                      />
-                    </div>
-                    {match.raidPoints > 0 && (
-                      <div className="stat_type">
-                        <h1>{match.raidPoints}</h1>
-                        <span>Raid Points</span>
-                      </div>
-                    )}
-                    {match.defensePoints > 0 && (
-                      <div className="stat_type">
-                        <h1>{match.defensePoints}</h1>
-                        <span>Tackle Points</span>
-                      </div>
-                    )}
-                    {match.totalPoints > 0 && (
-                      <div className="stat_type">
-                        <h1>{match.totalPoints}</h1>
-                        <span>Total Points</span>
-                      </div>
-                    )}
-                    {match.super10 > 0 && (
-                      <div className="stat_type">
-                        <h1>{match.super10}</h1>
-                        <span>Super 10</span>
-                      </div>
-                    )}
-                    {match.high5 > 0 && (
-                      <div className="stat_type">
-                        <h1>{match.high5}</h1>
-                        <span>High 5</span>
-                      </div>
-                    )}
-                    {match.superRaids > 0 && (
-                      <div className="stat_type">
-                        <h1>{match.superRaids}</h1>
-                        <span>Super Raids</span>
-                      </div>
-                    )}
+              return (
+                <div key={match.matchId} className="total_player_stats">
+                  <div className="opponent">
+                    <img src={player.team.logo} alt={player.team.logo} />
+                    <span>VS</span>
+                    <img
+                      src={match.opponentTeam.logo}
+                      alt={match.opponentTeam.name}
+                    />
                   </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
+                  {match.raidPoints > 0 && (
+                    <div className="stat_type">
+                      <h1>{match.raidPoints}</h1>
+                      <span>Raid Points</span>
+                    </div>
+                  )}
+                  {match.defensePoints > 0 && (
+                    <div className="stat_type">
+                      <h1>{match.defensePoints}</h1>
+                      <span>Tackle Points</span>
+                    </div>
+                  )}
+                  {match.totalPoints > 0 && (
+                    <div className="stat_type">
+                      <h1>{match.totalPoints}</h1>
+                      <span>Total Points</span>
+                    </div>
+                  )}
+                  {match.super10 > 0 && (
+                    <div className="stat_type">
+                      <h1>{match.super10}</h1>
+                      <span>Super 10</span>
+                    </div>
+                  )}
+                  {match.high5 > 0 && (
+                    <div className="stat_type">
+                      <h1>{match.high5}</h1>
+                      <span>High 5</span>
+                    </div>
+                  )}
+                  {match.superRaids > 0 && (
+                    <div className="stat_type">
+                      <h1>{match.superRaids}</h1>
+                      <span>Super Raids</span>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </>
       )}
-    </>
+    </div>
   );
 }
