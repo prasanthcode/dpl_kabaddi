3d1296aeeda7c51405a321541656dbfc63276501
Prasanth Gavvala <pintuprasanth9@gmail.com>
2025-10-08 15:08:00 +0530

    feat: add arrows for corousel

--- src/components/InstagramCarousel.js
+++ src/components/InstagramCarousel.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, IconButton } from "@mui/material";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { fetchCarouselGalleries } from "../services/galleryApi";
 
 export default function InstagramCarousel() {
   const [images, setImages] = useState([]);
+  const [sliderRef, setSliderRef] = useState(null);
 
   useEffect(() => {
     const fetchCarousel = async () => {
@@ -24,12 +26,12 @@ export default function InstagramCarousel() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows: true,
+    arrows: false, // we'll use custom arrows instead
   };
 
   return (
@@ -43,8 +45,10 @@ export default function InstagramCarousel() {
         boxShadow: 5,
         border: "1px solid #ddd",
         bgcolor: "white",
+        position: "relative",
       }}
     >
+      {/* Header */}
       <Box
         sx={{
           display: "flex",
@@ -60,31 +64,64 @@ export default function InstagramCarousel() {
         </Typography>
       </Box>
 
+      {/* Carousel */}
       {images.length > 0 ? (
-        <Slider {...settings}>
-          {images.map((item, index) => (
-            <Box
-              key={index}
-              sx={{
-                width: "100%",
-                height: 430,
-                position: "relative",
-                borderRadius: 3,
-              }}
-            >
-              <img
-                src={`${item.url}?w=350&h=400&fit=crop`}
-                alt={item.caption || `Slide ${index}`}
-                style={{
+        <Box sx={{ position: "relative" }}>
+          <Slider ref={setSliderRef} {...settings}>
+            {images.map((item, index) => (
+              <Box
+                key={index}
+                sx={{
                   width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                  borderRadius: "3px",
+                  height: 430,
+                  position: "relative",
                 }}
-              />
-            </Box>
-          ))}
-        </Slider>
+              >
+                <img
+                  src={`${item.url}?w=350&h=400&fit=crop`}
+                  alt={item.caption || `Slide ${index}`}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    borderRadius: "0 0 12px 12px",
+                  }}
+                />
+              </Box>
+            ))}
+          </Slider>
+
+          {/* Custom arrows */}
+          <IconButton
+            onClick={() => sliderRef?.slickPrev()}
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: 8,
+              transform: "translateY(-50%)",
+              color: "white",
+              "&:hover": { bgcolor: "rgba(0,0,0,0.6)" },
+              zIndex: 2,
+            }}
+          >
+            <ArrowBackIos sx={{ fontSize: 20 }} />
+          </IconButton>
+
+          <IconButton
+            onClick={() => sliderRef?.slickNext()}
+            sx={{
+              position: "absolute",
+              top: "50%",
+              right: 8,
+              transform: "translateY(-50%)",
+              color: "white",
+              "&:hover": { bgcolor: "rgba(0,0,0,0.6)" },
+              zIndex: 2,
+            }}
+          >
+            <ArrowForwardIos sx={{ fontSize: 20 }} />
+          </IconButton>
+        </Box>
       ) : (
         <Typography variant="body2" align="center" sx={{ p: 2, color: "gray" }}>
           No highlights available
